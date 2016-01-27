e914b34dcc0ea147b73708e58f14ecc30ee22f95
Scott Lovegrove <scottisafool@live.co.uk>
2016-01-27 17:17:43 +0000

    Initial commit

---
+++ LiveWriterPluginManager/ViewModel/RemovePluginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace LiveWriterPluginManager.ViewModel
+{
+    public class RemovePluginViewModel : ViewModelBase
+    {
+        public RemovePluginViewModel()
+        {
+            
+        }
+    }
+}
---
+++ LiveWriterPluginManager/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,42 @@
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace LiveWriterPluginManager.ViewModel
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Initializes a new instance of the ViewModelLocator class.
+        /// </summary>
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            ////}
+            ////else
+            ////{
+            ////    // Create run time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            ////}
+
+            SimpleIoc.Default.Register<AddPluginViewModel>();
+            SimpleIoc.Default.Register<RemovePluginViewModel>();
+        }
+
+        public AddPluginViewModel AddPlugin => ServiceLocator.Current.GetInstance<AddPluginViewModel>();
+        public RemovePluginViewModel RemovePlugin => ServiceLocator.Current.GetInstance<RemovePluginViewModel>();
+
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+    }
+}
